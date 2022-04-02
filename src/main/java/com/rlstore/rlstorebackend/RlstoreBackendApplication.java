cd6fc8e83a97309d9b0116a13fd1d1eec8710d50
lowaybasay <louiebasay@gmail.com>
2022-04-03 00:42:20 +0800

    initial setup

---
+++ src/main/java/com/rlstore/rlstorebackend/RlstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rlstore.rlstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rlstore/rlstorebackend/RlstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlstore.rlstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
